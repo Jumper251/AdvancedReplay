50c6702a153fc6726b1df58cc3ee14cf0e7b884b
Anton Müller <a.a.m.2502@googlemail.com>
2018-01-19 14:48:50 +0100

    Initital commit

---
+++ src/me/jumper251/replay/ReplaySystem.java
@@ -0,0 +1,16 @@
+package me.jumper251.replay;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ReplaySystem extends JavaPlugin{
+
+	@Override
+	public void onDisable() {
+		
+	}
+	
+	@Override
+	public void onEnable() {
+		
+	}
+}
