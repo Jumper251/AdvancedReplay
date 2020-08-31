9541096fefb753239acefd11c85644c88f4c898c
Jumper251 <a.a.m.2502@googlemail.com>
2020-09-01 00:27:50 +0200

    fixed error message

--- src/me/jumper251/replay/replaysystem/replaying/ReplayPacketListener.java
+++ src/me/jumper251/replay/replaysystem/replaying/ReplayPacketListener.java
@@ -79,7 +79,9 @@ public class ReplayPacketListener extends AbstractListener {
 	
 	@Override
 	public void unregister() {
-		ProtocolLibrary.getProtocolManager().removePacketListener(this.packetAdapter);
+		if (!VersionUtil.isAbove(VersionEnum.V1_16)) {
+			ProtocolLibrary.getProtocolManager().removePacketListener(this.packetAdapter);
+		}
 		
 	}
 	
